bdfadb2e68941c546a5eaa39692670e9a78b0232
saharatajim <sahara.tajim@gmail.com>
2026-06-01 22:25:42 +0600

    setup initialize

---
+++ index.js
@@ -0,0 +1,48 @@
+const dns = require("node:dns");  
+dns.setServers(["8.8.8.8", "8.8.4.4"]); 
+require('dotenv').config()   
+
+
+const express = require('express');
+const app = express();
+const port = 5000;
+
+
+const cors=require("cors")  
+app.use(cors())  
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello  World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example  listening on port ${port}`);
+});
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.DB_URI
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+  
+ const db=client.db("idea-vault")
+   const ideasCollection=db.collection("ideas")
+  
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
