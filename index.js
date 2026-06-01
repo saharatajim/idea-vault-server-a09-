0842ca5ac89c4e4a8a4f9a35d829215eb62ed1fd
saharatajim <sahara.tajim@gmail.com>
2026-06-02 01:44:03 +0600

    all idea shown-create operation done

--- index.js
+++ index.js
@@ -48,7 +48,11 @@ async function run() {
     res.json(result)
     }
     )
-
+//get all idea data on database    
+ app.get("/ideas",async(req,res)=>{
+    const allideas=await ideasCollection.find().toArray()
+    res.json(allideas)
+  })
 
   
     await client.db("admin").command({ ping: 1 });
